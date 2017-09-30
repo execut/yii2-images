1ec924e99ef7e0d7d6e9d6dbbb5bed02409b7e38
eXeCUT <sexecut@gmail.com>
2017-09-30 17:19:04 +0300

    Optimization

--- Attacher.php
+++ Attacher.php
@@ -8,23 +8,22 @@ namespace execut\images;
 use execut\yii\migration\Inverter;
 use execut\yii\migration\Migration;
 
-class Attacher extends Migration
+class Attacher extends \execut\yii\migration\Attacher
 {
     public $tables = [];
     public $sizes = [];
+    protected $attributes = [
+        'title',
+        'alt'
+    ];
+
+    protected function getVariations () {
+        return ['tables', 'sizes', 'attributes'];
+    }
+
     public function initInverter(Inverter $i) {
         foreach ($this->tables as $table) {
-            $cache = \yii::$app->cache;
-            $attributes = [
-                'title',
-                'alt'
-            ];
-
-            foreach ($attributes as $attribute) {
-                $cacheKey = __CLASS__ . '_' . $table . '_' . $attribute;
-                if ($cache->get($cacheKey)) {
-                    continue;
-                }
+            foreach ($this->attributes as $attribute) {
                 $tableSchema = $this->db->getTableSchema($table);
                 if (!$tableSchema) {
                     continue 2;
@@ -33,16 +32,9 @@ class Attacher extends Migration
                 if (!$tableSchema->getColumn($attribute)) {
                     $i->table($table)->addColumn($attribute, $this->string());
                 }
-
-                $cache->set($cacheKey, 1);
             }
 
             foreach ($this->sizes as $sizeName => $sizeParams) {
-                $cacheKey = __CLASS__ . '_' . $table . '_' . $sizeName;
-                if ($cache->get($cacheKey)) {
-                    continue;
-                }
-
                 $tableSchema = $this->db->getTableSchema($table);
                 if (!$tableSchema) {
                     continue 2;
@@ -55,8 +47,6 @@ class Attacher extends Migration
                     ]);
                     $helper->attach();
                 }
-
-                $cache->set($cacheKey, 1);
             }
         }
     }
