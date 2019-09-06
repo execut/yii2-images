c316ce5130954e0bf4441cd9241b31f7a58e8673
eXeCUT notebook <sexecut@gmail.com>
2019-09-06 10:25:23 +0300

    added id parameter

--- console/ConsoleController.php
+++ console/ConsoleController.php
@@ -7,12 +7,16 @@ use yii\console\Controller;
 
 class ConsoleController extends Controller
 {
-    public function actionIndex() {
+    public function actionIndex($fromId = null) {
         $filesModule = $this->module->getFilesModule();
         $modelClass = $filesModule->modelClass;
         $q = $modelClass::find()->withoutData([
             $filesModule->getColumnName('data')
-        ]);
+        ])->orderBy('id ASC');
+        if ($fromId !== null) {
+            $q->andWhere('id>' . $fromId);
+        }
+        
         foreach ($q->batch() as $files) {
             foreach ($files as $file) {
                 $file->scenario = Field::SCENARIO_FORM;
