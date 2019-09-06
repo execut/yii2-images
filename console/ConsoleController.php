530a137755863857cd6b7bc5719411a6710b8fb8
eXeCUT notebook <sexecut@gmail.com>
2019-09-06 10:41:03 +0300

    added info to command

--- console/ConsoleController.php
+++ console/ConsoleController.php
@@ -14,9 +14,7 @@ class ConsoleController extends Controller
         /**
          * @var ActiveQuery $q
          */
-        $q = $modelClass::find()->withoutData([
-            $filesModule->getColumnName('data')
-        ])->orderBy('id ASC');
+        $q = $modelClass::find()->orderBy('id ASC');
         if ($fromId !== null) {
             $q->andWhere('id>' . $fromId);
         }
@@ -25,14 +23,14 @@ class ConsoleController extends Controller
         $totalCount = $q->count();
         $this->stderr('Start resaving ' . $totalCount . " images\n");
         $currentCount = 0;
-        foreach ($q->batch(1) as $files) {
-            foreach ($files as $file) {
-                $file->scenario = Field::SCENARIO_FORM;
-                $file->save();
-                $this->stdout($file . ' is resaved' . "\n");
-                $currentCount++;
-                $this->stderr('Saved ' . $currentCount . ' from ' . $totalCount . "\n");
-            }
+        $ids = (clone $q)->select('id')->column();
+        foreach ($ids as $id) {
+            $file = $modelClass::findOne($id);
+            $file->scenario = Field::SCENARIO_FORM;
+            $file->save();
+            $this->stdout($file . ' is resaved' . "\n");
+            $currentCount++;
+            $this->stderr('Saved ' . $currentCount . ' from ' . $totalCount . "\n");
         }
     }
 }
